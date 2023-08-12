5454c2eee1ec0fbc5065fb363d5976bedb6d7b48
Jamie Twells <jamie@jamietwells.com>
2023-08-12 18:59:07 +0100

    Attributes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,75 @@
 ﻿askQuestion("Choose your gender", "Male", "Female");
 askQuestion("Choose your class", "Mage", "Warrior", "Rogue");
 askQuestion("Choose your weapon", "Staff", "Sword", "Spear");
+askNumber("What strength will your character have?");
+askNumber("What dexterity will your character have?");
+askNumber("What wisdom will your character have?");
+askNumber("What charisma will your character have?");
+
+int askNumber(string question)
+{
+    Console.WriteLine(question);
+
+    string value = Console.ReadLine();
+    bool valid = IsNumber(value);
+    while (!valid)
+    {
+        Console.WriteLine("Invalid number. Please try again");
+        value = Console.ReadLine();
+        valid = IsNumber(value);
+
+    }
+    int valueConverted = Convert.ToInt32(value);
+    return valueConverted;
+}
+
+bool IsNumber(string input)
+{
+    foreach (char item in input)
+    {
+        if (!IsCharNumber(item))
+            return false;
+    }
+    return true;
+}
+bool IsCharNumber(char input)
+{
+    if (input == '0')
+        return true;
+    if (input == '1')
+        return true;
+    if (input == '2')
+        return true;
+    if (input == '3')
+        return true;
+    if (input == '4')
+        return true;
+    if (input == '5')
+        return true;
+    if (input == '6')
+        return true;
+    if (input == '7')
+        return true;
+    if (input == '8')
+        return true;
+    if (input == '9')
+        return true;
+    return false;
+}
+
+
+bool arrayContains(string[] items, string item)
+{
+    foreach (string validOption in items)
+    {
+        if (item == validOption)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 
 
 string askQuestion(string question, params string[] options)
@@ -14,24 +83,5 @@ string askQuestion(string question, params string[] options)
         userOption = Console.ReadLine();
     }
     return userOption;
-
-    static bool arrayContains(string[] items, string item)
-    {
-        int currentOption = 0;
-
-        while (currentOption < items.Length)
-        {
-            string validOption = items[currentOption];
-            currentOption = currentOption + 1;
-
-
-            if (item == validOption)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
 }
 
