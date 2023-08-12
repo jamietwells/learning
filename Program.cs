df39d726136e625783e932978793a1f1d8abfd1d
Jamie Twells <jamie@jamietwells.com>
2023-08-12 14:54:04 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿askQuestion("Choose your gender", "Male", "Female");
+askQuestion("Choose your class", "Mage", "Warrior", "Rogue");
+askQuestion("Choose your weapon", "Staff", "Sword", "Spear");
+
+
+string askQuestion(string question, params string[] options)
+{
+    Console.WriteLine(question);
+    string userOption = Console.ReadLine();
+
+    while (!arrayContains(options, userOption))
+    {
+        Console.WriteLine("Incorrect option, pick again");
+        userOption = Console.ReadLine();
+    }
+    return userOption;
+
+    static bool arrayContains(string[] items, string item)
+    {
+        int currentOption = 0;
+
+        while (currentOption < items.Length)
+        {
+            string validOption = items[currentOption];
+            currentOption = currentOption + 1;
+
+
+            if (item == validOption)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
+
